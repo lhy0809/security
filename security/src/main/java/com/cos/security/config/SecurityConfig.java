1cc313e2bd40a31d7acbc458216810b290713534
hyong <hyong@hyong>
2024-08-13 22:31:53 +0900

    [refactor] security regacy 삭제

--- security/src/main/java/com/cos/security/config/SecurityConfig.java
+++ security/src/main/java/com/cos/security/config/SecurityConfig.java
@@ -12,24 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig /*extends WebSecurityConfigurerAdapter 구 버전에서 사용*/{
 	
-	/*
-	@Overrid
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeHttpRequests(authorizeRequests -> 
-			authorizeRequests
-				.antMatchers("/user/**").authenticated()
-				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-				// access : public AuthorizationManagerRequestMatcherRegistry access(AuthorizationManager<RequestAuthorizationContext> manager)
-				// string 타입의 인수를 전달할 수 없다.
-				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-				.anyRequest().permitAll()
-				.and()
-				.formLogin()
-				.loginPage("/login")
-		);
-	}
-	*/
+	
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
