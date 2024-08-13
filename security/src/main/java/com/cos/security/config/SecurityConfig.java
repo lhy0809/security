6a1630288b4300bfbd3833e3fd906ff33ec033c3
hyong <hyong@hyong>
2024-08-13 14:42:49 +0900

    [feat] 스프링 시큐리티 필터를 스프링 필터체인에 등록 - /user /manager /admin 권한 필요 설정 - 로그인 페이지 설정

---
+++ security/src/main/java/com/cos/security/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.cos.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig /*extends WebSecurityConfigurerAdapter 구 버전에서 사용*/{
+	
+	/*
+	@Overrid
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeHttpRequests(authorizeRequests -> 
+			authorizeRequests
+				.antMatchers("/user/**").authenticated()
+				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+				// access : public AuthorizationManagerRequestMatcherRegistry access(AuthorizationManager<RequestAuthorizationContext> manager)
+				// string 타입의 인수를 전달할 수 없다.
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+		);
+	}
+	*/
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.authorizeRequests(authorize -> authorize
+				.requestMatchers(new AntPathRequestMatcher("/user/**")).authenticated() // /user > 인증 필요
+				.requestMatchers(new AntPathRequestMatcher("/manager/**")).hasAnyRole("MANAGER", "ADMIN") // /manager > MANAGER, ADMIN 권한 필요
+				.requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN") // /admin > ADMIN 권한 필요
+				.anyRequest().permitAll() // 나머지 url > 권한 허용.
+				);
+
+		http.formLogin(form -> form
+				.loginPage("/login"));
+		// /user /manager /admin 으로 접근 시 403 error 발생 
+		// 단, formLogin.loginPage 를 지정하면 권한이 필요한 url에 대해서 /login 으로 이동.
+		return http.build();
+	}
+	
+}
--- security/src/main/java/com/cos/security/controller/IndexController.java
+++ security/src/main/java/com/cos/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,45 @@ public class IndexController {
 	@GetMapping({"","/"})
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/user")
+	@ResponseBody
+	public String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	@ResponseBody
+	public String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	@ResponseBody
+	public String manager() {
+		return "manager";
+	}
+	
+	/**
+	 * spring security 가 해당 주소를 가로챈다.
+	 * @return
+	 */
+	@GetMapping("/login")
+	@ResponseBody
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	@ResponseBody
+	public String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	@ResponseBody
+	public String joinProc() {
+		return "joinProc";
 	}
 }
