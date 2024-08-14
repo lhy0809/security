5dfbf25f6c60fd1f8aaf00e850a2623b62d19893
hyong <hyong@hyong>
2024-08-14 09:56:23 +0900

    [feat] security 권한처리 - @Secured, @PreAuthorize 어노테이션

--- security/src/main/java/com/cos/security/config/SecurityConfig.java
+++ security/src/main/java/com/cos/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig /*extends WebSecurityConfigurerAdapter 구 버전에서 사용*/{
 	
 	// @Bean 어노테이션은 해당 메서드의 리턴되는 오브젝트를 IoC 해준다.
--- security/src/main/java/com/cos/security/controller/IndexController.java
+++ security/src/main/java/com/cos/security/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.cos.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -65,5 +67,19 @@ public class IndexController {
 		user.setPassword(encryptPassword);
 		userRepository.save(user);
 		return "redirect:/loginForm";
+	}
+	
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/info")
+	@ResponseBody
+	public String info() {
+		return "개인정보";
+	}
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	@GetMapping("/data")
+	@ResponseBody
+	public String data() {
+		return "데이터정보";
 	}
 }
